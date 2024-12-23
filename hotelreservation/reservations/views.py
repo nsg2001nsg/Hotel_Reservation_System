0aedff3ff8ac88484485762a7b5c34859643d60a
surajsgupta0107 <surajsgupta0107@gmail.com>
2024-12-23 21:17:42 +0530

    first commit

--- hotelreservation/reservations/views.py
+++ hotelreservation/reservations/views.py
@@ -25,31 +25,30 @@ def reservation(reservation_id):
 def create_reservation(room_id):
     room = Room.query.filter_by(id=room_id).first_or_404()
     hotel = Hotel.query.filter_by(id=room.hotel_id).first_or_404()
-    last_reservation = Reservation.query.order_by(Reservation.id.desc()).first()
     form = ReservationForm()
-    number = f"{hotel.id}-{room.id}-{last_reservation.id + 1:02d}"
     if form.validate_on_submit():
         last_reservation = Reservation.query.order_by(Reservation.id.desc()).first()
-        number = f"{hotel.id}-{room.id}-{last_reservation.id+1:02d}"  # Reservation number format: hotel_id-room_number-reservation_number
-        # checkin_date = date_posted + timedelta(days=random.randint(0, 365))
-        # checkout_date = checkin_date + timedelta(days=random.randint(1, 8))
-        # reservation = Reservation(title=form.title.data, content=form.content.data, author=current_user, hotel_id=room_id.hotel_id, room_id=room.id)
-        # db.session.add(reservation)
-        # db.session.commit()
+        reservation = Reservation(
+            number=f"{hotel.id}-{room.id}-{last_reservation.id+1:02d}",  # Reservation number format: hotel_id-room_number-reservation_number
+            type=form.type.data,
+            checkin_date=form.checkin_date.data,
+            checkout_date=form.checkout_date.data,
+            guest_count=form.guest_count.data,
+            customer=current_user,
+            hotel=hotel,
+            room=room,
+        )
+        db.session.add(reservation)
+        db.session.commit()
         flash("Your reservation has been created.", "success")
         return redirect(url_for("main.home"))
     elif request.method == "GET":
-        checkin_date = datetime.date.today() + timedelta(days=8)
-        print(checkin_date)
-        # import ipdb; ipdb.set_trace()
+        checkin_date = datetime.date.today() + timedelta(days=1)
         if not bool(room.reservations.filter(Reservation.checkin_date == checkin_date).all()):
             reservation_type = reservation_types[0]  # Tentative
         else:
             reservation_type = reservation_types[1]  # Waitlisted
         form.type.data = reservation_type
-        form.checkin_date.data = checkin_date
-        # form.checkout_date.data = checkin_date + timedelta(days=1)
-        # form.guest_count.data = 1
     return render_template("create_reservation.html", title="Create Reservation", block_title="Create Reservation Page",
                            legend="Create Reservation Info", hotel=hotel, room=room, form=form)
 
